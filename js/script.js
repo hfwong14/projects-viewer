8069293c9522b8020418f520349ce57952a77cd7
hfwong14 <hfwong14@gmail.com>
2022-07-23 01:28:26 +0800

    Added Wan Chai Restaurant Random Picker

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 
-document.getElementById('LOL').innerText += "YES"
+var restNameList = [
+    '無所不用其極',
+    '煙味番茄醬',
+    '台灣野隔離',
+    '香港粉麵',
+    '雙喜燒味'
+]
+
+var restCount = restNameList.length
+
+function randomRest() {
+    randomIndex = Math.floor(Math.random() * restCount);
+    randomRestName = restNameList[randomIndex]
+    document.getElementById('restNameDisplay').innerText = randomRestName
+}
+
